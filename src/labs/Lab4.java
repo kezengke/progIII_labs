e432f5061eeaf9b3a00792e280432f2798c410ac
2kisa <2kisa@10.218.192.152>
2018-10-10 20:06:52 -0400

    added writeunique method and comments.

--- src/labs/Lab4.java
+++ src/labs/Lab4.java
@@ -5,16 +5,29 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
+/**
+ * a class to parse fasta file.
+ * @author 2kisa
+ *
+ */
 class FastaSequence
 {
 	private String header;
 	private StringBuffer sequence = new StringBuffer();
 	
-	
+	/**
+	 * return an ArrayList of FastaSeuquence object. each object contain a header and the corresponding sequence.
+	 * @param filepath file location
+	 * @return an ArrayList of FastaSequence obj.
+	 * @throws Exception
+	 */
 	public static List<FastaSequence> readFastaFile(String filepath) throws Exception
 	{
 		List<FastaSequence> fastaList = new ArrayList<FastaSequence>();
@@ -43,16 +56,53 @@ class FastaSequence
 			{
 				fs.sequence.append(nextLine);
 			}
+			
 		}
 		
 		
 		return fastaList;
 	}
 	
-	public static void writeUnique(String inFile, String outFile ) throws Exception
+	/**
+	 * writes each unique sequence to an output file with the 
+	 * number of times each sequence was seen in the input file as the header 
+	 * (sorted with the sequence seen the fewest times the first)
+	 * @param inFile input file path
+	 * @param outFile out put file path
+	 * @throws Exception
+	 */
+	public static void writeUnique(String inFile, String outFile) throws Exception
 	{
-		BufferedReader reader = new BufferedReader(new FileReader(new File(inFile)));
+		List<FastaSequence> fastaList = FastaSequence.readFastaFile(inFile);
 		BufferedWriter writer = new BufferedWriter(new FileWriter(new File(outFile)));
+		
+		HashMap<String, Integer> uniqueCountMap = new HashMap<String, Integer>();
+		
+		for( FastaSequence fs : fastaList)
+		{
+			Integer count = uniqueCountMap.get(fs.getSequence());
+			
+			if(count == null)
+			{
+				count = 0;
+			}
+			count ++;
+			
+			uniqueCountMap.put(fs.getSequence(),count);
+		}
+		
+		LinkedHashMap<String, Integer> sortedMap = new LinkedHashMap<>();
+		 
+		uniqueCountMap.entrySet().stream().sorted(Map.Entry.comparingByValue()).forEachOrdered(x -> sortedMap.put(x.getKey(), x.getValue()));
+		
+		Set<String> keys = sortedMap.keySet();
+		for(String key:keys)
+		{
+			writer.write(">" + sortedMap.get(key) + "\n" + key +"\n");
+		}
+		
+		writer.flush();
+		writer.close();
 		
 		
 	}
@@ -102,6 +152,12 @@ public class Lab4
 			System.out.println(fs.getSequence());
 			System.out.println(fs.getGCRatio());
 		}
+		
+		FastaSequence wuTest = new FastaSequence();
+		
+		wuTest.writeUnique("/Users/2kisa/programmingIII/wu_test.txt", "/Users/2kisa/programmingIII/sortedMap.txt");
 	}
+	
+	
 
 }
